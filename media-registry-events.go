99abcca7a4d6f87aa5bed193f3767c8635170519
Sergey Bolshchikov <sergey@bolshchikov.net>
2020-01-02 11:42:29 +0200

    write register into events

---
+++ media-registry-events.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/address"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/events"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1/state"
+)
+
+var SYSTEM = sdk.Export(_init)
+var PUBLIC = sdk.Export(registerMedia)
+var EVENTS = sdk.Export(imageRegistered)
+
+var OWNER_KEY = []byte("__CONTRACT_OWNER__")
+
+func _init() {
+	state.WriteBytes(OWNER_KEY, address.GetSignerAddress())
+}
+
+func imageRegistered() {}
+
+func registerMedia(pHash, imageURL, postedAt, copyrights, binaryHash string) {
+	events.EmitEvent(imageRegistered)
+}
+
+func main() {}
